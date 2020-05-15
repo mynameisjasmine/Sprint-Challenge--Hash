92b664c43c2523bc8b24dcd79989d7df452f8050
Jasmine <jasminecharles@hotmail.com>
2020-05-15 13:20:23 -0700

    latest

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -4,52 +4,31 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     #Convert array to dict with indexes as value
-    new_one = dict(map(reversed, enumerate(a)))
-    result = []
-     
-     if key in new_one.keys():
+    arr = dict(map(reversed, enumerate(a)))
+    result = {}
     
+    for key_i in arr:
+        index = 0
+        for key_j in arr:
+         
+            index += 1   
+            
+            if key_i + key_j == 0:
+                if key_i > 0:
+                    key = key_i
+                else:
+                    key = key_j
+                # print(key)
+                result[key] = abs(key)
+            
+            
+    # print(result)
 
-    return result
+    return list(result)
 
 
 
-# def find_pairs (arr):
 
-# check = len(arr)
-# pos_int = []
-# for i in range(check):
-    
-#     for j in range(i + 1, check):
-#         if abs(arr[i]) == abs(arr[j]):
-#             v.append(abs(arr[i]))
-#             # v.append(abs(arr[j]))
-        
-
-
-#             return pos_int
-
-
-# def find_while (arr):
-#     check = len(arr)
-#     v = []
-#     num = 0
-
-#     for i in range(check):
-#         if num < check:
-#             for j in range(i + 1, check):
-#                 #  if num < check:
-#                     if abs(arr[i]) == abs(arr[j]):
-#                         v.append(abs(arr[i]))
-                        
-#                         # v.append(abs(len(arr[i])))
-
-#                         # v.append(abs(arr[j]))
-
-#                         return v
-
-#         num += 1
-#         print('num count', num)
 
 
 
